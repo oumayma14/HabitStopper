676ecaa68d917cb7c4e0548ebab277d906c4f9a7
Oumayma <oumaima0fazani@gmail.com>
2026-02-20 02:24:56 +0100

    Refactor habit cards structure and styling

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.habitstopper
 
+import android.R
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,7 +20,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -27,108 +27,186 @@ import androidx.compose.ui.unit.dp
 import com.example.habitstopper.ui.theme.HabitStopperTheme
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material.icons.filled.LocalCafe
+import androidx.compose.material.icons.filled.Smartphone
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 
-class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
-        setContent {
-            HabitStopperTheme {
+data class HabitCard(
+    val name: String,
+    val icon: ImageVector,
+    val cardColor: Color
+)
+val habits = listOf(
+    HabitCard("No Sugar", Icons.Filled.LocalCafe, Color(0xFFFFF3E0)),
+    HabitCard("No Smoking", Icons.Filled.Whatshot, Color(0xFFFFEBEE)),
+    HabitCard("No DoomScrolling", Icons.Filled.Smartphone, Color(0xFFE3F2FD))
+)
 
-                // ---------- STREAK STATE ----------
-                var streak by remember { mutableStateOf(1) }
+@Composable
+fun HabitItemCard(
+    habit: HabitCard,
+    checked: Boolean,
+    onCheckedChange:(Boolean)-> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor=habit.cardColor
+        ),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = habit.icon,
+                    contentDescription = habit.name,
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+                Text(habit.name)
+            }
 
-                // ---------- DATE ----------
-                val today = LocalDate.now()
-                val dayName = today.format(DateTimeFormatter.ofPattern("EEEE"))
-                val dayNumber = today.dayOfMonth
-                val year = today.year
+            Checkbox(
+                checked = checked,
+                onCheckedChange = onCheckedChange
+            )
+        }
+    }
+}
+    class MainActivity : ComponentActivity() {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
 
-                fun getDaySuffix(day: Int): String {
-                    return if (day in 11..13) {
-                        "th"
-                    } else {
-                        when (day % 10) {
-                            1 -> "st"
-                            2 -> "nd"
-                            3 -> "rd"
-                            else -> "th"
+            setContent {
+                HabitStopperTheme {
+                    val checkedStates = remember { mutableStateMapOf<String, Boolean>() }
+
+                    // ---------- STREAK STATE ----------
+                    var streak by remember { mutableStateOf(0) }
+
+                    // ---------- DATE ----------
+                    val today = LocalDate.now()
+                    val dayName = today.format(DateTimeFormatter.ofPattern("EEEE"))
+                    val dayNumber = today.dayOfMonth
+                    val year = today.year
+
+                    fun getDaySuffix(day: Int): String {
+                        return if (day in 11..13) {
+                            "th"
+                        } else {
+                            when (day % 10) {
+                                1 -> "st"
+                                2 -> "nd"
+                                3 -> "rd"
+                                else -> "th"
+                            }
                         }
                     }
-                }
 
-                val formattedSecondLine =
-                    "$dayNumber${getDaySuffix(dayNumber)}, $year"
+                    val formattedSecondLine =
+                        "$dayNumber${getDaySuffix(dayNumber)}, $year"
 
-                // ---------- HEADER ----------
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 24.dp, top = 70.dp, end = 24.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Top
-                ) {
-
-                    // LEFT: Date
-                    Column(horizontalAlignment = Alignment.Start) {
-                        Text(
-                            text = dayName,
-                            style = MaterialTheme.typography.headlineMedium,
-                            fontWeight = FontWeight.Normal
-                        )
-                        Text(
-                            text = formattedSecondLine,
-                            style = MaterialTheme.typography.headlineMedium,
-                            fontWeight = FontWeight.Normal
-                        )
-                    }
-
-                    // RIGHT: Streak (click to increase)
-                    val streakColor =
-                        if (streak > 0) Color(0xFFFF9800)
-                        else MaterialTheme.colorScheme.onBackground
-
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.clickable {
-                            streak++ // tap to increase
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.Whatshot,
-                            contentDescription = "Streak",
-                            tint = streakColor,
-                            modifier = Modifier.size(50.dp)
-                        )
-
-                        Text(
-                            text = streak.toString(),
-                            style = MaterialTheme.typography.titleMedium,
-                            fontWeight = FontWeight.Normal,
-                            color = streakColor,
-                            modifier = Modifier.padding(start = 6.dp)
-                        )
-                    }
-
-
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 170.dp, start = 10.dp, end = 24.dp)
-                ) {
-                    Text(
-                        text = "What did you resist today?",
+                    Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 24.dp, start = 24.dp, end = 24.dp),
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.SemiBold
-                    )
+                            .padding(horizontal = 24.dp, vertical = 40.dp)
+                    ) {
+                        //HEADER (CONTAINS DATE AND STREAK)
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.Top
+                        ) {
+                            //DATE COLUMN
+                            Column {
+                                Text(dayName, style = MaterialTheme.typography.headlineSmall)
+                                Text(
+                                    formattedSecondLine,
+                                    style = MaterialTheme.typography.headlineSmall
+                                )
+                            }
+                            //STREAK ROW
+                            val streakColor =
+                                if (streak > 0) Color(0xFFFF9800)
+                                else MaterialTheme.colorScheme.onBackground
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.clickable { streak++ }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.Whatshot,
+                                    contentDescription = "Streak",
+                                    modifier = Modifier.size(50.dp),
+                                    tint = streakColor
+                                )
+                                Text(
+                                    text = streak.toString(),
+                                    modifier = Modifier.padding(start = 6.dp),
+                                    color = streakColor
+                                )
+                            }
+                        }
+
+                        Spacer(modifier = Modifier.height(40.dp))
+                        //TITLE
+                        Text(
+                            text = "What did you resist today ?",
+                            style = MaterialTheme.typography.titleLarge
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        //+ ADD HABIT BUTTON
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End
+                        ) {
+                            Text(
+                                text = "+ New Habit to Break",
+                                style = MaterialTheme.typography.titleSmall,
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(16.dp))
+                                    .background(Color(0xFFEAEAEA))
+                                    .clickable {}
+                                    .padding(vertical = 10.dp, horizontal = 16.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        //CARDS
+                        Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+                            habits.forEach { habit ->
+                                val checked = checkedStates[habit.name] ?: false
+
+                                HabitItemCard(
+                                    habit = habit,
+                                    checked = checked,
+                                    onCheckedChange = { checkedStates[habit.name] = it }
+                                )
+                            }
+                        }
+
+                    }
                 }
             }
         }
     }
-}
